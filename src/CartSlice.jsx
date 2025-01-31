ae54714b71d0318591c1d1e3bdc573e306db2e9b
OlivierR63 <olivier.rochat@gmail.com>
2025-01-31 09:17:16 -0500

    implemented reducers addItem, removeItem and updateQuantity

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -5,17 +5,57 @@ export const CartSlice = createSlice({
   initialState: {
     items: [], // Initialize items as an empty array
   },
-  reducers: {
-    addItem: (state, action) => {
-    
-    },
-    removeItem: (state, action) => {
-    },
-    updateQuantity: (state, action) => {
 
-    
+    reducers: {
+        addItem: (state, action) => {
+            const {name, image, cost} = action.payload;
+            const existingItem = state.items.find(item => item.name == name);
+
+            // Remark : The existingItem variable points to the same object as the element found in state.items.
+            //         It does not create a copy of the object, but simply a reference to that object.
+            
+            if(existingItem){
+                existingItem.quantity ++;
+            }
+            else{
+                state.items.push({name, image, cost, quantity:1});
+            }
+        },
+
+        removeItem: (state, action) => {
+            const {name} = action.payload;
+            const existingItem = state.items.find(item => item.name === name);
+
+            if(existingItem && existingItem.quantity > 0){
+                existingItem.quantity --;
+            }
+
+            if (existingItem && existingItem.quantity == 0){
+                const itemIndex = state.items.indexOf(existingItem);
+                if (itemIndex != -1){
+                    state.items=state.items.splice(itemIndex, 1);
+                }
+            }
+        },
+
+        updateQuantity: (state, action) => {
+            const { name, quantity } = action.payload;
+            const itemToUpdate = state.items.find(item => item.name === name);
+            
+            if (itemToUpdate){
+                if( quantity > 0){
+                    itemToUpdate.quantity = quantity;
+                }
+
+                if (quantity == 0){
+                    const itemIndex = state.items.indexOf(itemToUpdate);
+                    if (itemIndex != -1){
+                        state.items=state.items.splice(itemIndex, 1);
+                    }
+                }
+            }   
+        },
     },
-  },
 });
 
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
